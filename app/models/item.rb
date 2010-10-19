c0358f0d3dd54188e9f7de3959442ed9f4938e5a
Zuppr <zuppr@yahoo.co.uk>
2010-10-19 20:51:53 +0100

    Set hex using alpha_numeric instead of old hex.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ActiveRecord::Base
 
-  after_create :set_hex
+  after_create :set_alpha_numeric
  
-  def set_hex
-    self.hex = (self.id+15).to_s(16)
+  def set_alpha_numeric
+    self.alpha_numeric = (self.id+15).to_s(16)
     self.save
   end
 
