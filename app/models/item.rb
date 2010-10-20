a7a4cb2f68dee331e5723235bfe084c4484259aa
Zuppr <zuppr@yahoo.co.uk>
2010-10-20 10:33:25 +0100

    Changed hex i.e. 16 to alphanumeric i.e. 62 in Item model and added 10000000 to id.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ActiveRecord::Base
   after_create :set_alpha_numeric
  
   def set_alpha_numeric
-    self.alpha_numeric = (self.id+15).to_s(62)
+    self.alpha_numeric = (self.id+10000000).to_s(62)
     self.save
   end
 
